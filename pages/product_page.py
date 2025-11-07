70339c72a396836707b6355ebcbb93b160d17c26
Артем Куканов <artemkukanov@MacBook-Air-Artem.local>
2025-11-07 13:05:46 +0300

    Урок 4 блока 4.3

---
+++ pages/product_page.py
@@ -0,0 +1,7 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+class ProductPage(BasePage):
+    def add_product_to_basket(self):
+        add_btn = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET)
+        add_btn.click()
