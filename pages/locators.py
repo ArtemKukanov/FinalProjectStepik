a300747e41d5e080be26752f2a54ea8401ae5e55
Артем Куканов <artemkukanov@MacBook-Air-Artem.local>
2025-11-07 16:28:43 +0300

    Урок 4, блок 4.3 - все работает, добавлено комментирование

--- pages/locators.py
+++ pages/locators.py
@@ -1,15 +1,18 @@
 from selenium.webdriver.common.by import By
 
+"""Класс локаторов, относящихся к URL логина"""
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
+"""Класс локаторов, относящихся к странице регистрации"""
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
 
+"""Класс локаторов, относящихся к странице продукта"""
 class ProductPageLocators():
-    ADD_TO_BASKET = (By.CSS_SELECTOR, ".btn.btn-lg.btn-primary.btn-add-to-basket")
+    ADD_TO_BASKET = (By.CSS_SELECTOR, ".btn.btn-lg.btn-primary.btn-add-to-basket") # кнопка добавления товара в корзину
     PRODUCT_NAME = (By.CSS_SELECTOR, ".col-sm-6 h1") # название со страницы
-    ALERT_NAME = (By.CSS_SELECTOR, "#messages .alert:nth-child(1) .alertinner>strong") # название из алерта
+    ALERT_NAME = (By.CSS_SELECTOR, "#messages .alert:nth-child(1) .alertinner strong") # название из алерта
     PRODUCT_PRICE = (By.CSS_SELECTOR,"p.price_color") # цена из со страницы
     ALERT_PRICE = (By.CSS_SELECTOR, "#messages .alert:nth-child(3) .alertinner strong") # цена из алерта
--- test_product_page.py
+++ test_product_page.py
@@ -4,6 +4,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from .pages.product_page import ProductPage
 
+"""Параметризация запуска"""
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
@@ -11,13 +12,13 @@ from .pages.product_page import ProductPage
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
+                                  pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail), # marks=pytest.mark.xfail -- параметр помечается как ожидающий сбой (xfail — expected failure)
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
 def test_guest_can_add_product_to_basket(browser, link):
-    page = ProductPage(browser, link)
-    page.open()
-    page.add_product_to_basket()
-    page.solve_quiz_and_get_code()
-    page.verification__product_name()
-    page.verification__product_price()
+    page = ProductPage(browser, link) # создается объект page, принадлежащий классу ProductPage (ProductPage -- наследник BasePage), page принимает все "def" из ProductPage в product_page.py
+    page.open() # страница (page) открывается с помощью функции (def open) принадлежащей классу BasePage, описанному в base_page.py (но технически def open принадлежит и дочернему классу ProductPage)
+    page.add_product_to_basket() # тестоввый метод add_product_to_basket производит действия над page
+    page.solve_quiz_and_get_code() # тестоввый метод solve_quiz_and_get_code производит действия над page
+    page.verification__product_name() # тестоввый метод verification__product_name производит действия над page
+    page.verification__product_price() # тестоввый метод verification__product_price производит действия над page
